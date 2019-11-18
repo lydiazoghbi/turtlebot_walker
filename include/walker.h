6fc233c8eb4b90f40d691727c483bf8252273f32
lydiazoghbi <lalzogbi@terpmail.umd.edu>
2019-11-17 20:56:38 -0800

    [walker.h] added first structure of the walker header

---
+++ include/walker.h
@@ -0,0 +1,19 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "geometry_msgs/Twist.h"
+#include <iostream>
+
+class Walker {
+	public:
+		Walker();
+		void Explore(const int& vel);
+		geometry_msgs::Twist MoveStraight(const double& forwardVelocity);
+		geometry_msgs::Twist Rotate(const double& angle);
+
+	private:
+		ros::NodeHandle nh_;
+		ros::Publisher pub_;
+		static int msg_rate_;
+		geometry_msgs::Twist velocity_;
+};
+
